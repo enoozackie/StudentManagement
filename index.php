24c449be0271505643dc59507ac79f6075518f84
Lourd Ian Akol <lourdianrentinoakol@gmail.com>
2025-07-22 15:56:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
+use akol\StudentManagement\Core\Database;
+use Akol\StudentManagement\Model\StudentModel;
+
+
 include 'vendor/autoload.php';
 
-Use Akol\StudentManagement\Model\StudentModel;
 
-$student = new StudentModel;
-$student-> id = 2024611053;
-$student-> fullname = "lourd ian";
-$student-> yearlevel = "FirstYear";
-$student-> course = "BSIT 1";
-$student-> section = "A";
-
-$student-> DisplayInfo();
+$students = new StudentModel;
+$students  = $student->read();
+var_dump($students);
